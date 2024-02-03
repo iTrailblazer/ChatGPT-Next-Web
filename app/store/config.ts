ba527c1671677c216550de5af369d5830213152b
chenqianhe <1278095698@qq.com>
2024-02-04 02:51:38 +0800

    Support longer messages

--- app/store/config.ts
+++ app/store/config.ts
@@ -138,7 +138,7 @@ export const useAppConfig = createPersistStore(
 
       if (version < 3.4) {
         state.modelConfig.sendMemory = true;
-        state.modelConfig.historyMessageCount = 4;
+        state.modelConfig.historyMessageCount = 99999;
         state.modelConfig.compressMessageLengthThreshold = 1000;
         state.modelConfig.frequency_penalty = 0;
         state.modelConfig.top_p = 1;
